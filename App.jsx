a62e59b6072b18788c5cb394c51e5b58f2636682
Mark Nemmer <marknemmer@gmail.com>
2024-04-07 18:39:31 -0400

    Initialize firebase project.

--- App.jsx
+++ App.jsx
@@ -2,10 +2,11 @@ import { SETTINGS_THEME_APPEARANCE_KEY } from '@constants/storage-keys';
 import BleManagerProvider from '@contexts/ble-manager/BleManagerProvider';
 import { usePersistentState } from '@hooks/storage-hooks';
 import MainTabs from '@navigation/MainTabs';
+import { firebase } from '@react-native-firebase/auth';
 import { ThemeProvider } from '@rneui/themed';
 import { genRneTheme } from '@styles/theme';
 import { StatusBar } from 'expo-status-bar';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useColorScheme } from 'react-native';
 import KeyboardManager from 'react-native-keyboard-manager';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -19,6 +20,15 @@ export default function App() {
   KeyboardManager.setEnable(true);
   const [colorScheme] = usePersistentState(SETTINGS_THEME_APPEARANCE_KEY, { defaultValue: useColorScheme() });
   const rneTheme = useMemo(() => genRneTheme(colorScheme), [colorScheme]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((usr) => {
+      console.log(usr);
+      setUser(usr);
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <SafeAreaProvider>
---
+++ functions/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    'ecmaVersion': 2018,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    'no-restricted-globals': ['warn', 'name', 'length'],
+    'prefer-arrow-callback': 'warn',
+    'quotes': ['warn', 'single', { 'allowTemplateLiterals': true }],
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.*'],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
