4c56daa05202fca8c7283ba2c65866a3a998a6eb
Mark Nemmer <marknemmer@gmail.com>
2024-06-18 13:26:07 -0400

    Complete PhoneInput component tests.

--- components/email-input/__tests__/EmailInput.test.tsx
+++ components/email-input/__tests__/EmailInput.test.tsx
@@ -1,8 +1,73 @@
 import EmailInput from '@components/email-input/EmailInput';
+import Form from '@components/form/Form';
+import { EMAIL_PATTERN_RULE } from '@constants/validation';
+import { genMockForm } from '@hooks/__mocks__/form-hooks';
 import AppProvider from '@test/contexts/app/AppProvider';
-import { render, screen } from '@testing-library/react-native';
+import { act, render, screen, userEvent } from '@testing-library/react-native';
+import { cloneElement } from 'react';
 
 describe('<EmailInput />', () => {
+  describe('validation', () => {
+    it('should show validation error with invalid email', async () => {
+      const form = genMockForm({
+        emailInput: '',
+      });
+
+      const renderJSX = (
+        <Form form={form}>
+          <EmailInput name="emailInput" />
+        </Form>
+      );
+
+      render(
+        renderJSX,
+        { wrapper: AppProvider }
+      );
+
+      const textInput = screen.getByTestId('RNE__Input__text-input');
+      await userEvent.type(textInput, 'testuser@domain');
+
+      await act(() =>
+        form.handleSubmit(() => {})()
+      );
+
+      expect(form.formState.errors.emailInput.type).toBe('pattern');
+
+      screen.rerender(
+        cloneElement(renderJSX, { form })
+      );
+
+      const errorText = screen.getByText(EMAIL_PATTERN_RULE.message);
+      expect(errorText).toBeVisible();
+    });
+
+    it('should not show validation error with valid email', async () => {
+      const form = genMockForm({
+        emailInput: '',
+      });
+
+      const renderJSX = (
+        <Form form={form}>
+          <EmailInput name="emailInput" />
+        </Form>
+      );
+
+      render(
+        renderJSX,
+        { wrapper: AppProvider }
+      );
+
+      const textInput = screen.getByTestId('RNE__Input__text-input');
+      await userEvent.type(textInput, 'testuser@domain.com');
+
+      await act(() =>
+        form.handleSubmit(() => {})()
+      );
+
+      expect(form.formState.errors.emailInput).toBeUndefined();
+    });
+  });
+
   describe('snapshots', () => {
     it('renders correctly', () => {
       render(
--- constants/validation.ts
+++ constants/validation.ts
@@ -1,10 +1,10 @@
 import { EMAIL_REGEX, PHONE_REGEX } from '@constants/regex';
-import { ValidationRule } from 'react-hook-form';
+import type { ValidationValueMessage } from 'react-hook-form';
 
 /**
  * The default email pattern {@link ValidationRule}.
  */
-export const EMAIL_PATTERN_RULE: ValidationRule<RegExp> = {
+export const EMAIL_PATTERN_RULE: ValidationValueMessage<RegExp> = {
   value: EMAIL_REGEX,
   message: 'Invalid email address',
 };
@@ -17,7 +17,7 @@ export const PASSWORD_MIN_LENGTH = 6;
 /**
  * The default password minimum length {@link ValidationRule}.
  */
-export const PASSWORD_MIN_LENGTH_RULE: ValidationRule<number> = {
+export const PASSWORD_MIN_LENGTH_RULE: ValidationValueMessage<number> = {
   value: PASSWORD_MIN_LENGTH,
   message: `Password must be at least ${PASSWORD_MIN_LENGTH} characters`,
 };
@@ -25,7 +25,7 @@ export const PASSWORD_MIN_LENGTH_RULE: ValidationRule<number> = {
 /**
  * The default phone pattern {@link ValidationRule}.
  */
-export const PHONE_PATTERN_RULE: ValidationRule<RegExp> = {
+export const PHONE_PATTERN_RULE: ValidationValueMessage<RegExp> = {
   value: PHONE_REGEX,
   message: 'Invalid phone number',
 };
